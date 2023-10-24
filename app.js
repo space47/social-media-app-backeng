dc5f493c589b85053aa0e65a79886b9d46229be6
Varnit Gupta <varnitgupta47@gmail.com>
2023-10-24 15:31:44 +0530

    new commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express=require('express');
 const app=express();
 const cookieParser=require('cookie-parser');
+const { connectDatabase } = require('./config/database');
+const cloudinary=require('cloudinary');
+require('dotenv').config()
 
 if(process.env.NODE_ENV!=="production"){
     require("dotenv").config({path:"env/.env"});
 }
 
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
 // using middleware
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit:"50mb",extended:true}))
@@ -22,4 +31,26 @@ app.get('/',(req,res) => {
 app.use("/api/v1",post);
 app.use("/api/v1",user);
 
-module.exports=app;    
+
+
+const port = process.env.PORT || 3000
+const start = async () => {
+    try {
+        connectDatabase();
+        app.listen(port,()=>{
+            console.log(`Server is running on ${process.env.PORT}`);
+        })
+        
+    } catch (error) {
+        console.log(error)        
+    }
+}
+
+start()
+
+
+
+
+
+
+
